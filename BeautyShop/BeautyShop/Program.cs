611fd26f68f6cde07e131e7e7a0e041963e93b4a
Zgurova <63356958+Zgurova@users.noreply.github.com>
2020-04-12 19:17:33 +0300

    Add files via upload

---
+++ BeautyShop/BeautyShop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BeautyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
